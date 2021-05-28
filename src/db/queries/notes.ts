6fd59a7c677e974423154b6a0fca9cf38911619f
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-05-28 11:55:24 -0500

    Quick fixes to notes API before deployment

--- src/db/queries/notes.ts
+++ src/db/queries/notes.ts
@@ -6,4 +6,4 @@ export const get_note = (id: string, user_id: string) => Query<Note[]>('SELECT *
 
 export const create_note = (new_note: Note) => Query('INSERT INTO Notes SET ?', 'notes', [new_note]);
 export const update_note = ({ content, id, user_id }: Note) => Query('UPDATE Notes SET content=? WHERE id=? AND user_id=?', 'notes', [content, id, user_id]);
-export const destroy_note = (id: string, user_id: string) => Query('DELETE FROM notes WHERE id=? AND user_id=?', 'notes', [id, user_id]);
+export const destroy_note = (id: string, user_id: string) => Query('DELETE FROM Notes WHERE id=? AND user_id=?', 'notes', [id, user_id]);
