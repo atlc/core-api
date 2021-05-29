1b67f119e1eafd9333df582ec7ef8f97541b89e5
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-05-28 21:30:17 -0500

    Fixing CORS rules to include muliple possible domains

--- src/middlewares/configure.ts
+++ src/middlewares/configure.ts
@@ -10,16 +10,20 @@ import './passport-strategies';
 
 
 export const configure = (app: express.Application) => {
-    app.use(passport.initialize());
-    app.use(express.json());
-    app.use(morgan('dev'));
-    app.use(helmet());
-    app.use(compression());
+  app.use(passport.initialize());
+  app.use(express.json());
+  app.use(morgan('dev'));
+  app.use(helmet());
+  app.use(compression());
 
-    app.use(cors({
-        "origin": config.cors.approvedDomains,
-        "methods": config.cors.methods,
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
-      }));
+
+
+  app.use(cors({
+    origin: (origin, callback) => {
+      config.cors.approvedDomains.includes(origin) ? callback(null, true) : new Error('Origin not approved by CORS')
+    },
+    methods: config.cors.methods,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  }));
 }
