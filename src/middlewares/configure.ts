f928825a1f90ddbf65e64035f9290a42d227c1a8
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-06-05 17:08:29 -0500

    Retightening up CORS

--- src/middlewares/configure.ts
+++ src/middlewares/configure.ts
@@ -5,6 +5,20 @@ import * as compression from 'compression';
 import * as helmet from 'helmet';
 import * as cors from 'cors';
 import './passport-strategies';
+import * as config from '../config';
+
+//@ts-ignore
+const corsOptionsDelegate =  function (req, cb) {
+  const corsOptions = {
+    origin: false
+  }
+
+  if (config.cors.approvedDomains.indexOf(req.header('Origin')) !== -1) {
+    corsOptions['origin'] = true;
+  }
+
+  cb(null, corsOptions);
+}
 
 
 export const configure = (app: express.Application) => {
@@ -13,7 +27,5 @@ export const configure = (app: express.Application) => {
   app.use(morgan('dev'));
   app.use(helmet());
   app.use(compression());
-
-  app.use(cors());
-  app.options('*', cors());
+  app.use(cors(corsOptionsDelegate));
 }
