18765aa1c397345c4cfbe6a23ed1d3fa22cb83d7
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-10-26 17:22:02 -0500

    Adding in links to my other sites at GET request of my API's root endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,4 +7,12 @@ const router = Router();
 router.use("/v1", apiRouter);
 router.use("/auth", authRouter);
 
+router.get("/", (req, res) => {
+    const other_links: { site: string; url: string }[] = [
+        { site: "main portfolio site", url: "https://atlc.dev" },
+        { site: "covid dashboard lite", url: "https://covid.atlc.dev" }
+    ];
+    res.json({ message: "There's nothing on the root of this API but I do have some other sites if you're interested!", other_links });
+});
+
 export default router;
